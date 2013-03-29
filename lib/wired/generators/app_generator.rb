0968f64a33ae2e65656c369bfbe02a1d489b9d1c
Bart van Zon <bartvanzon@gmail.com>
2013-03-29 12:07:34 +0100

    correctly invoke the file_removals

--- lib/wired/generators/app_generator.rb
+++ lib/wired/generators/app_generator.rb
@@ -8,5 +8,38 @@ module Wired
 
     class_option :skip_test_unit, :type => :boolean, :aliases => '-T', :default => true,
       :desc => 'Skip Test::Unit files'
+
+    def finish_template
+      invoke :wired_customization
+      super
+    end
+
+    def wired_customization
+      invoke :remove_files_we_dont_need
+      invoke :outro
+    end
+
+    def remove_files_we_dont_need
+      build :remove_public_index
+      build :remove_rails_logo_image
+    end
+
+    def outro
+      say 'Wired up!'
+    end
+
+    def run_bundle
+      # Let's not: We'll bundle manually at the right spot
+    end
+
+    protected
+
+    def get_builder_class
+      Wired::AppBuilder
+    end
+
+    def using_active_record?
+      !options[:skip_active_record]
+    end
   end
 end
