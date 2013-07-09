36d62e32d7e232212976ffb84797292332133dfb
Arne <arne@wirelab.nl>
2013-07-09 13:58:58 +0200

    Revert "Merge branch 'refs/heads/feature/google-analytics' into develop"
    This reverts commit 3941fe9bc9358403eb0b4440b19f3ba64c9f3f64, reversing
    changes made to eda34b13a3b80c36409c18f6a6cb54306e920756.

--- lib/wired/generators/app_generator.rb
+++ lib/wired/generators/app_generator.rb
@@ -83,7 +83,6 @@ module Wired
       say 'Creating views'
       build :create_partials_directory
       build :create_shared_flashes
-      build :create_google_analytics
       build :create_application_layout
     end
 
